7c4056e046ddb2fe1c86331b3e0265ea8e7e3a0a
FaizaMuquaddas <faizamuquaddas12@gmail.com>
2024-08-08 17:24:29 +0530

    added data types

---
+++ 02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; /* using this will treat all JS code as newer version
+
+alert(3+3)  //we are using nodejs, not browser meaning it might work when we are using JS with html and css but not when we are using solely JS
+
+console.log(3 +
+        3)         //  this eg. was used to demonstrate that code readablity should be high 
+
+Number
+String  => ""
+BigInt
+Boolean  => it is a logical entity which is either true/false
+Symbol => unique
+null      
+undefined   
+*/
+
+
+let name = "Faiza"
+let age = 25
+let isLoggedIn = false
+let state 
+
+console.log(typeof "Faiza")
+console.log(typeof null) //output will be object ; people have diff opinions on its output some might say it is an error by JS
+console.log(typeof undefined) // output undefined 
+
+
+
+
