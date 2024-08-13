d0d59f2938f6da803af07f0b281cf4dcf9fbdcb8
FaizaMuquaddas <faizamuquaddas12@gmail.com>
2024-08-13 22:17:16 +0530

    added string along with new and old syntax

---
+++ 01_basics/string.js
@@ -0,0 +1,9 @@
+const name = "faiza"
+const age = 25
+
+console.log(name + " " + age + " " + "edu-UG"); //outdated syntax not a good practice
+
+console.log(`Hello! My name is ${name.toLocaleUpperCase()}. I am ${age} years old.`); /*``  these back ticks are called string interpolation using this is 
+                                                                                        a modern way of writing using string. we can also use methods in it too as we did in name*/
+
+
