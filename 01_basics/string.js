5156f233fab0aed11a1f8b1bd59f1e2ee16f2190
FaizaMuquaddas <faizamuquaddas12@gmail.com>
2024-08-18 22:11:56 +0530

    added strings and notes file

--- 01_basics/string.js
+++ 01_basics/string.js
@@ -6,4 +6,9 @@ console.log(name + " " + age + " " + "edu-UG"); //outdated syntax not a good pra
 console.log(`Hello! My name is ${name.toLocaleUpperCase()}. I am ${age} years old.`); /*``  these back ticks are called string interpolation using this is 
                                                                                         a modern way of writing using string. we can also use methods in it too as we did in name*/
 
+//but using .toUpperCase() or lower wont make changes to original stringrather it will make changes in the copy of the string
 
+console.log(name.charAt(2)); //gives the charcter present on the particular index eg 2 in this
+console.log(name.indexOf('z')); //checks the psoditon of the character
+
+const gameName = new String(`hello`)
