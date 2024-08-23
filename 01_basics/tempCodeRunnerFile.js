3c0a36141bb2f5d4e03b05fb9d7eab78caf250fe
FaizaMuquaddas <faizamuquaddas12@gmail.com>
2024-08-23 20:01:37 +0530

    updates temp

--- 01_basics/tempCodeRunnerFile.js
+++ 01_basics/tempCodeRunnerFile.js
@@ -1,1 +1,3 @@
-console.log(Math.max(4,5,3,8));
+console.log(newDate.getDate());
+// console.log(newDate.getMonth() + 1);  // using +1 is important because JS 
+// console.log(newDate.getFullYear());
