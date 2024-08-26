7d9f95de22c8ae96ab73a3ca6e043d9028338ebb
FaizaMuquaddas <faizamuquaddas12@gmail.com>
2024-08-26 22:19:40 +0530

    nested objects

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,20 @@
+const Youtube_User = new Object()
+
+// const Youtube_User2 {
+// }
+
+Youtube_User.Id = "Faiza123"
+Youtube_User.name = "Faiza Muquaddas"
+Youtube_User.isSubscriber = true
+
+console.log(Youtube_User);
+
+const regular_user = {
+    email: "faiza@gmaul.com",
+    full_name:{
+        user_fullname:{
+            firstname: "Faiza",
+            lastname: "Muquaddas"
+        }
+    }
+}
