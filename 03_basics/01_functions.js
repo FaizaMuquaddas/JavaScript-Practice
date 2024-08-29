8e20cca9231ac97629db5b64e5a34f162e6f262b
FaizaMuquaddas <faizamuquaddas12@gmail.com>
2024-08-29 20:16:23 +0530

    functions and if else

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -9,8 +9,26 @@ function sayMyName(){
 // add two numbers
 
 function add_two_num(num1, num2){  //function defination parameters (num1, num2)
-    console.log(num1 + num2); 
+    // console.log(num1 + num2); 
+    // let result = num1 + num2
+    // return result
+    return num1 + num2
+
 }
 //arguments: passing values
-add_two_num(2,8) // add_two_num(2."3") :- "3" will be treated as a number
+const result = add_two_num(2,8) // add_two_num(2."3") :- "3" will be treated as a number
+// console.log("Result: ", result);
+
+
+
+function loginUser_message(username = "User"){  // = "User" is given a default value which can be orride if we pass new value later
+    if(!username){  //if(username === undefined) 
+        console.log("Please enter a username");
+        return
+    }
+    return `${username} just logged in`
+}
+
+// console.log(loginUser_message("Faiza"));
+console.log(loginUser_message());  //output-> undefined if value is not given
 
