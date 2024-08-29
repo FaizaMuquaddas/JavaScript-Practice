1b0b8c7402ff32e558fb30e9ba93a4cc50c5ab2c
FaizaMuquaddas <faizamuquaddas12@gmail.com>
2024-08-29 19:21:58 +0530

    added functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,16 @@
+function sayMyName(){
+    console.log("Faiza");
+    console.log("Muquaddas");
+    console.log("FM");
+}
+
+// sayMyName()
+
+// add two numbers
+
+function add_two_num(num1, num2){  //function defination parameters (num1, num2)
+    console.log(num1 + num2); 
+}
+//arguments: passing values
+add_two_num(2,8) // add_two_num(2."3") :- "3" will be treated as a number
+
