b2af7096ce77f50accf54de7b04e48938b870231
FaizaMuquaddas <faizamuquaddas12@gmail.com>
2024-08-29 20:44:27 +0530

    different types of parameters passing in function

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -32,3 +32,48 @@ function loginUser_message(username = "User"){  // = "User" is given a default v
 // console.log(loginUser_message("Faiza"));
 console.log(loginUser_message());  //output-> undefined if value is not given
 
+
+
+// passing different types of parameters
+// example in shopping apps user keeps adding unpredicated items to it and we have to keep adding accordingly so, in this situtation we dont know how many arguments to give and parameters to give
+
+function calculate_cartPrice(...num1){  //... means rest operator depends on use case it is also spread
+return num1
+}
+
+// console.log(calculate_cartPrice(200, 400, 500, 800));
+
+
+// Q. Interview question sometimes asks 
+// function calc(val1, val2, ...num1){
+// }
+// console.log(calculate_cartPrice(200, 400, 500, 800));
+// where these (val1, val2, ...num1) values will go
+
+// ANS. so 200 and 400 will go in val1 and val2,  rest values will go in array of ...num1
+
+
+const user = {
+    item: "bag",
+    price: 999
+}
+
+function handleObject(anyObject){
+    console.log(`Username is ${anyObject.item} and price is ${anyObject.price}`);
+}
+
+// handleObject(user)
+handleObject({
+    item: "chocolate",
+    price: 299
+})
+
+// can also pass arrays
+const myNewArray = [200, 300, 400, 500]
+
+function returnSecondValue(getArray){
+    return getArray[2]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([100, 200, 300, 400]));
