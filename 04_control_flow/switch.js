fdd0216ae48ef96438bf54da44b6e7d96d8b3a90
FaizaMuquaddas <faizamuquaddas12@gmail.com>
2024-08-31 21:58:35 +0530

    switch case

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,26 @@
+const month = 3
+
+switch(month){
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("feburary");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    case 4:
+        console.log("april");
+        break;
+    case 5:
+        console.log("may");
+        break;
+
+        default:
+            console.log("Non");
+            
+            break;
+        
+}
+
