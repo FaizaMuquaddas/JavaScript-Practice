3e37204c1a3750689c30601dad95984f18a3c9ed
FaizaMuquaddas <faizamuquaddas12@gmail.com>
2024-09-02 22:41:58 +0530

    added for-each

---
+++ 05_iterations.js/for-each_loop.js
@@ -0,0 +1,51 @@
+// useful for iterating arrays
+
+const coding = ["js", "Java", "C", "cpp", "Rust", "Python"]
+
+coding.forEach( function (item) {   //function will bring value from an array inside it as a pramater itself so defining name of a function is not necessary
+        // console.log(item);
+} )
+
+
+
+// we can use arrow function as well
+coding.forEach( (value) => {
+    // console.log(value);
+} )
+
+
+
+// or
+function printme(item){
+    // console.log(item);
+    
+}
+
+// coding.forEach(printme)
+
+
+
+
+coding.forEach( (item, index, arr ) => {   //for each can have more access of paramters
+        // console.log(item, index, arr);
+} )
+
+
+const myCoding = [
+    {
+        languageName: "JavaScript",
+        languageFileName: "js",
+    },
+    {
+        languageName: "Java",
+        languageFileName: "java",
+    },
+    {
+        languageName: "Python",
+        languageFileName: "py",
+    }
+]
+
+myCoding.forEach( (item) => {       //each item is Object in each iteration
+    console.log(item.languageName);
+})
