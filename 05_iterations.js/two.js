a5e500bd09e9d2702d5210191902c60176e33ace
FaizaMuquaddas <faizamuquaddas12@gmail.com>
2024-09-02 20:17:11 +0530

    added while and do while

---
+++ 05_iterations.js/two.js
@@ -0,0 +1,23 @@
+
+let i = 0;
+while(i<=10){
+    // console.log(`Value of index is ${i}`);
+    i = i+2
+}
+
+let myarr = ["flash", "Superman", "batman"]
+
+let arr = 0
+while(arr < myarr.length){
+    // console.log(`Value is ${myarr[arr]}`);
+    arr++
+}
+
+// DO WHILE
+
+let score = 10
+do{
+    console.log(`Score is ${score}`);
+    score++
+}
+while(score <= 10);
